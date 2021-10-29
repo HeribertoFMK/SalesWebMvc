dd2422302b9853db7c0d9c469f9dff28591ad2b7
HeribertoFMK <hfmk2015@gmail.com>
2021-10-29 10:55:22 +0900

    Criado a tela Simples de Cadastro

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Model;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Saller> FindAll()
+        {
+           return  _context.Saller.ToList();
+        }
+        public void Insert(Saller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+    }
+}
