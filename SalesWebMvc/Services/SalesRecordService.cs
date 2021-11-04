2332fbe15fa6319dcdcdd88bd33f4196ed25f070
HeribertoFMK <hfmk2015@gmail.com>
2021-11-04 20:27:39 +0900

    Implementado a Busca Simples

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,35 @@
+﻿using SalesWebMvc.Model;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SalesRecord>> FindByDateAsysc(DateTime? minDate,DateTime? maxDate)
+        {
+            var Result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+              Result = Result.Where(x => x.Date >= minDate.Value);
+
+            }
+            if (maxDate.HasValue)
+            {
+                Result = Result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await Result.Include(x => x.Saller).Include(x => x.Saller.Department).OrderByDescending(x=> x.Date).ToListAsync();
+
+        }
+    }
+}
